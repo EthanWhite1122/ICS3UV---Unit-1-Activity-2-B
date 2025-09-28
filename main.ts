691a6a34ac0bf16ab7446d04919cbacbe0bafb26
Ethan White <ethan.white1@stu.ocsb.ca>
2025-09-28 17:31:55 +0000

    Created activity B and finished Activity B

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Ethan White
+ * @version 1.0.0
+ * @date 2025-09-28
+ * @fileoverview This progrm will print my name "Ethan White".
+ */
+
+//print my name
+console.log("Ethan White!")
+
+console.log("\nDone")
